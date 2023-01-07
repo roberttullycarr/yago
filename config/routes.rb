ee17ac75dd9f6ba7150d30b123541bbda4ab0789
Robert Carr <robert@pitchdrive.com>
2023-01-07 12:00:50 +0100

    added a root page to api to test rendering

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: "main#index"
+
   namespace :api do
     namespace :v1 do
       post 'authentication/create'
