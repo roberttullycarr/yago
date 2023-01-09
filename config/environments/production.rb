7877a8308eb2c7e8b89468b90fa61c085b499cc2
Robert Carr <robert@pitchdrive.com>
2023-01-09 09:37:58 +0100

    fixed host url issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'yago-frontend-ms326sg9p-roberttullycarr.vercel.app' }
+  config.action_mailer.default_url_options = { host: 'yago-frontend.vercel.app/' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -99,7 +99,7 @@ Rails.application.configure do
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'yago-frontend-ms326sg9p-roberttullycarr.vercel.app'
+      origins 'yago-frontend.vercel.app/'
       resource '*', headers: :any, methods: %i[get post patch put delete destroy], credentials: true
 
       origins '*'
